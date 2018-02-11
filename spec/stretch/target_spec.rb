32c563ca6d9e91c33f3e997f56beef59459cf474
wate <pc.poisoning@gmail.com>
2018-02-11 19:23:09 +0900

    Squashed 'provision/roles/apt-backports/' content from commit b36389a
    git-subtree-dir: provision/roles/apt-backports
    git-subtree-split: b36389aa603a0a16a0c8e7020598caf2ceb945b9

---
+++ spec/stretch/target_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe file('/etc/apt/sources.list.d/backports.list') do
+  it { should contain 'deb http://ftp.debian.org/debian stretch-backports main' }
+end
+describe file('/etc/apt/apt.conf.d/99target') do
+  it { should exist }
+  it { should be_file }
+  it { should contain 'APT::Default-Release stretch-backports;' }
+end
