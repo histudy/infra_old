5fc77c83d9a89c6315107d239a44389bd30b0f39
wate <pc.poisoning@gmail.com>
2018-02-22 22:53:11 +0900

    Squashed 'provision/roles/nginx/' changes from 4fc71e9..1dc8730
    1dc8730 サンプルコードの注記を追記
    4424cbb ロールの移し込み
    d5f6e28 とりあえず実装
    
    git-subtree-dir: provision/roles/nginx
    git-subtree-split: 1dc8730fa128de15d874d05bd55292d1e8b85d4e

--- README.md
+++ README.md
@@ -6,11 +6,16 @@ nginx
 Role Variables
 --------------
 
-| 変数名         | 内容                                          |
-| -------------- | --------------------------------------------- |
-| nginx_packages | インストールするNginxのパッケージを設定します |
+| 変数名                | 内容                                                   |
+| --------------------- | ------------------------------------------------------ |
+| nginx_packages        | インストールするNginxのパッケージを設定します          |
+| nginx_cfg             | httpディレクティブの内容を設定します                   |
+| nginx_vhosts          | serverディレクティブの内容を設定します                 |
+| nginx_generate_dh_key | Diffie-Hellman鍵共有用の鍵を生成するか否かを設定します |
+| nginx_dh_key_bit      | Diffie-Hellman鍵のサイズを設定します                   |
+| nginx_dh_key_path     | Diffie-Hellman鍵の保存先を設定します                   |
 
-###
+### nginx_packages
 
 インストールするNginxのパッケージを設定します。
 
@@ -21,10 +26,138 @@ nginx_packages:
   - nginx
 ```
 
-Dependencies
-------------
+### nginx_cfg
 
-* [apt-backports](https://github.com/histudy/ansible-role-apt-backports)
+httpディレクティブの内容を設定します。
+
+#### Example
+
+```yml
+nginx_cfg:
+  user: "www-data"
+  worker_processes: auto
+  pid: /run/nginx.pid
+  events:
+    worker_connections: 1024
+  http:
+    sendfile: yes
+    tcp_nopush: yes
+    tcp_nodelay: yes
+    keepalive_timeout: 65
+    types_hash_max_size: 2048
+    server_tokens: no
+    default_type: "application/octet-stream"
+    access_log: /var/log/nginx/access.log
+    error_log: /var/log/nginx/error.log
+    client_max_body_size: 8M
+    gzip: yes
+    gzip_vary: yes
+    gzip_proxied: any
+    gzip_comp_level: 6
+    gzip_buffers: "16 8k"
+    gzip_http_version: 1.1
+    gzip_types:
+      - text/plain
+      - text/css
+      - application/json
+      - application/javascript
+      - text/xml
+      - application/xml
+      - application/xml+rss
+      - text/javascript
+```
+
+### nginx_vhosts
+
+serverディレクティブの内容を設定します。
+
+#### Example
+
+※以下、指定可能な全項目の設定例です
+
+```yml
+nginx_vhosts:
+  - name: default
+    default: yes
+    server_name: www.exampe.com
+    # server_name:
+    #   - exampe.com
+    #   - "*.exampe.com"
+    access_log: /var/log/nginx/access.log
+    error_log: /var/log/nginx/error.log
+    document_root: /var/www/html
+    client_max_body_size: 16M
+    index: index.html
+    # index:
+    #   - index.html
+    #   - index.htm
+    #   - index.php
+    variables:
+      variable_name: varriable_value
+    includes:
+      - snippets/wordpress.conf
+    locations:
+      - pattern: "/\\.ht"
+        match_type: "~"
+        content: "deny all;"
+    ssl:
+      certificate: /path/to/cert.pem
+      certificate_key:  /path/to/privkey.pem
+      trusted_certificate: /path/to/chain.pem
+      protocols:
+        - TLSv1.2
+      ciphers:
+        - ECDHE-ECDSA-AES256-GCM-SHA384
+        - ECDHE-RSA-AES256-GCM-SHA384
+        - ECDHE-ECDSA-CHACHA20-POLY1305
+        - ECDHE-RSA-CHACHA20-POLY1305
+        - ECDHE-ECDSA-AES128-GCM-SHA256
+        - ECDHE-RSA-AES128-GCM-SHA256
+        - ECDHE-ECDSA-AES256-SHA384
+        - ECDHE-RSA-AES256-SHA384
+        - ECDHE-ECDSA-AES128-SHA256
+        - ECDHE-RSA-AES128-SHA256
+      session_timeout: "1d"
+      session_cache: "shared:SSL:50m"
+      session_tickets: no
+      hsts: "max-age=15768000; includeSubDomains;"
+      stapling: yes
+      stapling_verify: yes
+    extra_setting: |
+      # some extra setting here
+```
+
+### nginx_generate_dh_key
+
+Diffie-Hellman鍵共有用の鍵を生成するか否かを設定します。
+
+#### Example
+
+```yml
+nginx_generate_dh_key: yes
+```
+
+### nginx_dh_key_bit
+
+Diffie-Hellman鍵のサイズを設定します。  
+※nginx_generate_dh_keyに`true`が設定されている場合のみ有効です。
+
+#### Example
+
+```yml
+nginx_dh_key_bit: 2048
+```
+
+### nginx_dh_key_path
+
+Diffie-Hellman鍵の保存先を設定します。  
+※nginx_generate_dh_keyに`true`が設定されている場合のみ有効です。
+
+#### Example
+
+```yml
+nginx_dh_key_path: /etc/nginx/dhparam.pem
+```
 
 Example Playbook
 ----------------
---
+++ spec/config_spec.rb
@@ -0,0 +1,132 @@
+require 'spec_helper'
+
+describe file('/etc/nginx/nginx.conf') do
+  it { should exist }
+  it { should be_file }
+
+  its(:content) { should match(/^user #{e(property['nginx_cfg']['user'])};$/) }
+
+  worker_processes_value = e(property['nginx_cfg']['worker_processes'])
+  its(:content) { should match(/^worker_processes #{worker_processes_value};$/) }
+
+  pid_value = e(property['nginx_cfg']['pid'])
+  its(:content) { should match(/^pid #{pid_value};$/) }
+
+  property['nginx_cfg']['events'].each do |key, value|
+    its(:content) { should match(/^\s*#{e(key)} #{e(value)};$/) }
+  end
+
+  sendfile_value = property['nginx_cfg']['http']['sendfile'] ? 'on' : 'off'
+  its(:content) { should match(/^\s*sendfile #{sendfile_value};$/) }
+
+  tcp_nopush_value = property['nginx_cfg']['http']['tcp_nopush'] ? 'on' : 'off'
+  its(:content) { should match(/^\s*tcp_nopush #{tcp_nopush_value};$/) }
+
+  tcp_nodelay_value = property['nginx_cfg']['http']['tcp_nodelay'] ? 'on' : 'off'
+  its(:content) { should match(/^\s*tcp_nodelay #{tcp_nodelay_value};$/) }
+
+  keepalive_timeout_value = e(property['nginx_cfg']['http']['keepalive_timeout'])
+  its(:content) { should match(/^\s*keepalive_timeout #{keepalive_timeout_value};$/) }
+  types_hash_max_size = e(property['nginx_cfg']['http']['types_hash_max_size'])
+  its(:content) { should match(/^\s*types_hash_max_size #{types_hash_max_size};$/) }
+
+  if property['nginx_cfg']['http'].key?('server_tokens')
+    server_tokens_value = property['nginx_cfg']['http']['server_tokens'] ? 'on' : 'off'
+    its(:content) { should match(/^\s*server_tokens #{server_tokens_value};$/) }
+  end
+  if property['nginx_cfg']['http'].key?('server_names_hash_bucket_size')
+    server_names_hash_bucket_size_value = e(property['nginx_cfg']['http']['server_names_hash_bucket_size'])
+    its(:content) { should match(/^\s*server_names_hash_bucket_size #{server_names_hash_bucket_size_value};$/) }
+  end
+  if property['nginx_cfg']['http'].key?('server_name_in_redirect')
+    server_name_in_redirect_value = property['nginx_cfg']['http']['server_name_in_redirect'] ? 'on' : 'off'
+    its(:content) { should match(/^\s*server_name_in_redirect #{server_name_in_redirect_value};$/) }
+  end
+
+  default_type_value = e(property['nginx_cfg']['http']['default_type'])
+  its(:content) { should match(/^\s*default_type #{default_type_value};$/) }
+
+  if property['nginx_cfg']['http'].key?('client_max_body_size')
+    client_max_body_size_value = e(property['nginx_cfg']['http']['client_max_body_size'])
+    its(:content) { should match(/^\s*client_max_body_size #{client_max_body_size_value};$/) }
+  end
+  # Logging Settings
+  access_log_value = e(property['nginx_cfg']['http']['access_log'])
+  its(:content) { should match(/^\s*access_log #{access_log_value};$/) }
+
+  error_log_value = e(property['nginx_cfg']['http']['error_log'])
+  its(:content) { should match(/^\s*error_log #{error_log_value};$/) }
+
+  # SSL Settings
+  ssl_protocols_value = e(property['nginx_cfg']['http']['ssl']['protocols'].join(' '))
+  its(:content) { should match(/^\s*ssl_protocols #{ssl_protocols_value};$/) }
+
+  ssl_ciphers_value = e(property['nginx_cfg']['http']['ssl']['ciphers'].join(':'))
+  its(:content) { should match(/^\s*ssl_ciphers "#{ssl_ciphers_value}";$/) }
+
+  ssl_prefer_server_ciphers_value = property['nginx_cfg']['http']['ssl']['prefer_server_ciphers'] ? 'on' : 'off'
+  its(:content) { should match(/^\s*ssl_prefer_server_ciphers #{ssl_prefer_server_ciphers_value};$/) }
+
+  session_timeout_value = e(property['nginx_cfg']['http']['ssl']['session_timeout'])
+  its(:content) { should match(/^\s*ssl_session_timeout #{session_timeout_value};$/) }
+
+  ssl_session_cache_value = e(property['nginx_cfg']['http']['ssl']['session_cache'])
+  its(:content) { should match(/^\s*ssl_session_cache #{ssl_session_cache_value};$/) }
+
+  ssl_session_tickets_value = property['nginx_cfg']['http']['ssl']['session_tickets'] ? 'on' : 'off'
+  its(:content) { should match(/^\s*ssl_session_tickets #{ssl_session_tickets_value};$/) }
+
+  ssl_stapling_value = property['nginx_cfg']['http']['ssl']['stapling'] ? 'on' : 'off'
+  its(:content) { should match(/^\s*ssl_stapling #{ssl_stapling_value};$/) }
+
+  ssl_stapling_verify_value = property['nginx_cfg']['http']['ssl']['stapling_verify'] ? 'on' : 'off'
+  its(:content) { should match(/^\s*ssl_stapling_verify #{ssl_stapling_verify_value};$/) }
+
+  if property['nginx_cfg']['http']['ssl'].key?('dhparam')
+    ssl_dhparam_value = e(property['nginx_cfg']['http']['ssl']['dhparam'])
+    its(:content) { should match(/^\s*ssl_dhparam #{ssl_dhparam_value};$/) }
+  end
+
+  if property['nginx_cfg']['http']['ssl'].key?('hsts')
+    hsts_value = e(property['nginx_cfg']['http']['ssl']['hsts'])
+    its(:content) { should match(/^\s*add_header Strict-Transport-Security "#{hsts_value}";$/) }
+  end
+
+  # Gzip Settings
+  gzip_value = property['nginx_cfg']['http']['gzip'] ? 'on' : 'off'
+  its(:content) { should match(/^\s*gzip #{gzip_value};$/) }
+
+  gzip_vary_value = property['nginx_cfg']['http']['gzip_vary'] ? 'on' : 'off'
+  its(:content) { should match(/^\s*gzip_vary #{gzip_vary_value};$/) }
+
+  gzip_proxied_value = e(property['nginx_cfg']['http']['gzip_proxied'])
+  its(:content) { should match(/^\s*gzip_proxied #{gzip_proxied_value};$/) }
+
+  gzip_comp_level = e(property['nginx_cfg']['http']['gzip_comp_level'])
+  its(:content) { should match(/^\s*gzip_comp_level #{gzip_comp_level};$/) }
+
+  gzip_buffers_value = e(property['nginx_cfg']['http']['gzip_buffers'])
+  its(:content) { should match(/^\s*gzip_buffers #{gzip_buffers_value};$/) }
+
+  gzip_http_version_value = e(property['nginx_cfg']['http']['gzip_http_version'])
+  its(:content) { should match(/^\s*gzip_http_version #{gzip_http_version_value};$/) }
+
+  gzip_types_value = e(property['nginx_cfg']['http']['gzip_types'].join(' '))
+  its(:content) { should match(/^\s*gzip_types #{gzip_types_value};$/) }
+
+  its(:content) { should match(/^\s*# nginx.conf extra setting$/) }
+end
+
+%w[redirect_https.conf redirect_www.conf wordpress.conf].each do |snippet|
+  describe file('/etc/nginx/snippets/' + snippet) do
+    it { should exist }
+    it { should be_file }
+  end
+end
+
+if property['nginx_generate_dh_key']
+  describe file(property['nginx_dh_key_path']) do
+    it { should exist }
+    it { should be_file }
+  end
+end
