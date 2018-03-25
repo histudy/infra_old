b08fa6997d29ef032a7a1328897811b7d8786972
wate <pc.poisoning@gmail.com>
2018-03-25 17:15:46 +0900

    Squashed 'provision/roles/common/' changes from 9b899bf..bb4c9f8
    bb4c9f8 テストを修正
    
    git-subtree-dir: provision/roles/common
    git-subtree-split: bb4c9f80e243d77a084715a35891706e6a668ea5

--- spec/ssh_spec.rb
+++ spec/ssh_spec.rb
@@ -5,7 +5,7 @@ describe file('/usr/local/bin/sshfilter') do
   it { should be_file }
   it { should be_executable }
   it { should be_mode 755 }
-  it { should contain 'ALLOW_COUNTRIES="JP"' }
+  it { should contain "ALLOW_COUNTRIES=\"#{property['common_ssh_allow_countries'].join(' ')}\"" }
 end
 
 describe file('/usr/local/bin/update_geoip_database') do
@@ -13,4 +13,28 @@ describe file('/usr/local/bin/update_geoip_database') do
   it { should be_file }
   it { should be_executable }
   it { should be_mode 755 }
+end
+
+describe file('/etc/hosts.deny') do
+  if property['common_ssh_use_geoip_filter']
+    its(:content) { should match(/^sshd: ALL/) }
+  else
+    its(:content) { should_not match(/^sshd:/) }
+  end
+end
+
+describe file('/etc/hosts.allow') do
+  if property['common_ssh_use_geoip_filter']
+    its(:content) { should match(/^sshd:/) }
+  else
+    its(:content) { should_not match(/^sshd:/) }
+  end
+end
+
+describe file('/etc/cron.d/ansible_management_job') do
+  if property['common_ssh_use_geoip_filter']
+    it { should contain '/usr/local/bin/update_geoip_database' }
+  else
+    it { should_not contain '/usr/local/bin/update_geoip_database' }
+  end
 end
--- spec/sudo_spec.rb
+++ spec/sudo_spec.rb
@@ -3,4 +3,9 @@ require 'spec_helper'
 describe file('/etc/sudoers.d/admin') do
   it { should exist }
   it { should be_file }
+  if property['common_requre_sudo_password']
+    its(:content) { should_not match(/NOPASSWD:/) }
+  else
+    its(:content) { should match(/NOPASSWD:/) }
+  end
 end
