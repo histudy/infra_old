cc243135fa24ebfeb056352b2eee9f3737fb61d9
wate <pc.poisoning@gmail.com>
2018-03-25 18:50:33 +0900

    deep_mergeのオプション指定を共通化

--- provision/spec/spec_helper.rb
+++ provision/spec/spec_helper.rb
@@ -23,6 +23,10 @@ role_names = ENV['ROLE_NAMES'].split(',')
 
 ansible_vars = {}
 
+deep_merge_option = {
+  overwrite_arrays: true
+}
+
 find_file_patterns = []
 role_paths.each do |role_path|
   find_file_patterns << role_path + '/{' + role_names.join(',') + '}/defaults/main.yml'
@@ -31,17 +35,17 @@ end
 variable_files = Dir.glob(find_file_patterns)
 variable_files.each do |role_default_variable_file|
   role_default_vars = YAML.load_file(role_default_variable_file)
-  ansible_vars.deep_merge!(role_default_vars, {:overwrite_arrays => true}) if role_default_vars.is_a?(Hash)
+  ansible_vars.deep_merge!(role_default_vars, deep_merge_option) if role_default_vars.is_a?(Hash)
 end
 
 trim_string = host + ' | SUCCESS => '
 dump = `ansible -m setup #{host}`
 dump.slice!(0, trim_string.length)
-ansible_vars.deep_merge!(YAML.load(dump)['ansible_facts'], {:overwrite_arrays => true})
+ansible_vars.deep_merge!(YAML.load(dump)['ansible_facts'], deep_merge_option)
 
 dump = `ansible -m debug -a 'var=vars' #{host}`
 dump.slice!(0, trim_string.length)
-ansible_vars.deep_merge!(YAML.load(dump)['vars'], {:overwrite_arrays => true})
+ansible_vars.deep_merge!(YAML.load(dump)['vars'], deep_merge_option)
 
 find_file_patterns = []
 role_paths.each do |role_path|
@@ -50,7 +54,7 @@ end
 variable_files = Dir.glob(find_file_patterns)
 variable_files.each do |role_variable_file|
   role_vars = YAML.load_file(role_variable_file)
-  ansible_vars.deep_merge!(role_vars, {:overwrite_arrays => true}) if role_vars.is_a?(Hash)
+  ansible_vars.deep_merge!(role_vars, deep_merge_option) if role_vars.is_a?(Hash)
 end
 
 set_property ansible_vars
