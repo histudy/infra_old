95b0adafbb6c52fc2bc04eda0ef5547169c93019
wate <pc.poisoning@gmail.com>
2018-05-15 02:59:53 +0900

    リポジトリの管理情報を追加

---
+++ terraform/repositories.tf
@@ -0,0 +1,84 @@
+resource github_repository "role-common" {
+  name          = "ansible-role-common"
+  has_issues    = true
+  has_downloads = false
+}
+resource github_repository "role-nginx" {
+  name          = "ansible-role-nginx"
+  has_issues    = true
+  has_downloads = false
+}
+resource github_repository "role-mariadb" {
+  name          = "ansible-role-mariadb"
+  has_issues    = true
+  has_downloads = false
+}
+
+resource github_repository "role-php" {
+  name          = "ansible-role-php"
+  has_issues    = true
+  has_downloads = false
+}
+resource github_repository "role-python" {
+  name          = "ansible-role-python"
+  has_issues    = true
+  has_downloads = false
+}
+resource github_repository "role-dehydrated" {
+  name          = "ansible-role-dehydrated"
+  has_issues    = true
+  has_downloads = false
+}
+resource github_repository "role-lexicon" {
+  name          = "ansible-role-lexicon"
+  has_issues    = true
+  has_downloads = false
+}
+
+resource github_repository "role-mackerel-agent" {
+  name          = "ansible-role-mackerel-agent"
+  has_issues    = true
+  has_downloads = false
+}
+
+resource github_repository "role-apt-backports" {
+  name          = "ansible-role-apt-backports"
+  has_issues    = true
+  has_downloads = false
+}
+
+
+/**
+ * Unused
+ */
+
+resource github_repository "role-nodejs" {
+  name          = "ansible-role-nodejs"
+  has_issues    = true
+  has_downloads = false
+}
+resource github_repository "role-java" {
+  name          = "ansible-role-java"
+  has_issues    = true
+  has_downloads = false
+}
+resource github_repository "role-minecraft-server" {
+  name          = "ansible-role-minecraft-server"
+  has_issues    = true
+  has_downloads = false
+}
+
+/**
+ * work in progress
+ */
+
+ resource github_repository "role-apache" {
+   name          = "ansible-role-apache"
+   has_issues    = true
+   has_downloads = false
+ }
+ resource github_repository "role-postgresql" {
+   name          = "ansible-role-postgresql"
+   has_issues    = true
+   has_downloads = false
+ }
