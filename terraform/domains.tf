a9215dba8dc7a055db85d686e0eefc0baf546788
wate <pc.poisoning@gmail.com>
2019-01-16 13:55:56 +0900

    リポジトリやドメインの設定を更新

--- terraform/domains.tf
+++ terraform/domains.tf
@@ -1,14 +1,14 @@
 resource cloudflare_record "at" {
   domain = "histudy.jp"
   name   = "@"
-  value  = "203.104.205.229"
+  value  = "133.223.7.42"
   type   = "A"
 }
 
 resource cloudflare_record "www" {
   domain = "histudy.jp"
   name   = "www"
-  value  = "203.104.205.229"
+  value  = "133.223.7.42"
   type   = "A"
 }
 
@@ -24,19 +24,4 @@ resource cloudflare_record "mail_secondary" {
   name   = "mail"
   type   = "NS"
   value  = "ns2.dns.ne.jp"
-}
-
-resource cloudflare_record "postmaster_tool_auth" {
-  domain = "histudy.jp"
-  name   = "@"
-  type   = "TXT"
-  ttl    = "86400"
-  value  = "google-site-verification=7PjGDFZDUN-c85npu9gYvkj1DN0SVGnRBVhQQDDv5nY"
-}
-
-resource cloudflare_record "vyos" {
-  domain = "histudy.jp"
-  name   = "vyos"
-  value  = "133.223.1.217"
-  type   = "A"
 }
--- terraform/repositories.tf
+++ terraform/repositories.tf
@@ -1,53 +1,71 @@
 resource github_repository "role-common" {
   name          = "ansible-role-common"
+  description   = "サーバの共通設定などをセットアップするロールです"
+  homepage_url  = "https://histudy.jp/"
   has_issues    = true
   has_downloads = false
 }
 
 resource github_repository "role-nginx" {
   name          = "ansible-role-nginx"
+  description   = "Nginxをセットアップするロールです"
+  homepage_url  = "https://histudy.jp/"
   has_issues    = true
   has_downloads = false
 }
 
 resource github_repository "role-mariadb" {
   name          = "ansible-role-mariadb"
+  description   = "MariaDBをセットアップするロールです"
+  homepage_url  = "https://histudy.jp/"
   has_issues    = true
   has_downloads = false
 }
 
 resource github_repository "role-php" {
   name          = "ansible-role-php"
+  description   = "PHPをセットアップするロールです"
+  homepage_url  = "https://histudy.jp/"
   has_issues    = true
   has_downloads = false
 }
 
 resource github_repository "role-python" {
   name          = "ansible-role-python"
+  description   = "Pythonをセットアップするロールです"
+  homepage_url  = "https://histudy.jp/"
   has_issues    = true
   has_downloads = false
 }
 
 resource github_repository "role-dehydrated" {
   name          = "ansible-role-dehydrated"
+  description   = "dehydrated(Let's Encrypt)をセットアップするロールです"
+  homepage_url  = "https://histudy.jp/"
   has_issues    = true
   has_downloads = false
 }
 
 resource github_repository "role-lexicon" {
   name          = "ansible-role-lexicon"
+  description   = "lexiconをセットアップするロールです"
+  homepage_url  = "https://histudy.jp/"
   has_issues    = true
   has_downloads = false
 }
 
 resource github_repository "role-mackerel-agent" {
   name          = "ansible-role-mackerel-agent"
+  description   = "Mackerelエージェントをセットアップするロールです"
+  homepage_url  = "https://histudy.jp/"
   has_issues    = true
   has_downloads = false
 }
 
 resource github_repository "role-apt-backports" {
   name          = "ansible-role-apt-backports"
+  description   = "Debian Backportsをセットアップするロールです"
+  homepage_url  = "https://histudy.jp/"
   has_issues    = true
   has_downloads = false
 }
@@ -58,36 +76,48 @@ resource github_repository "role-apt-backports" {
 
 resource github_repository "role-nodejs" {
   name          = "ansible-role-nodejs"
+  description   = "Node.jsをセットアップするロールです"
+  homepage_url  = "https://histudy.jp/"
   has_issues    = true
   has_downloads = false
 }
 
 resource github_repository "role-java" {
   name          = "ansible-role-java"
+  description   = "Javaをセットアップするロールです"
+  homepage_url  = "https://histudy.jp/"
   has_issues    = true
   has_downloads = false
 }
 
 resource github_repository "role-minecraft-server" {
   name          = "ansible-role-minecraft-server"
+  description   = "minecraftサーバをセットアップするロールです"
+  homepage_url  = "https://histudy.jp/"
   has_issues    = true
   has_downloads = false
 }
 
 resource github_repository "role-fluentd" {
   name          = "ansible-role-fluentd"
+  description   = "Fluentdをセットアップするロールです"
+  homepage_url  = "https://histudy.jp/"
   has_issues    = true
   has_downloads = false
 }
 
 resource github_repository "role-ruby" {
   name          = "ansible-role-ruby"
+  description   = "Rubyをセットアップするロールです"
+  homepage_url  = "https://histudy.jp/"
   has_issues    = true
   has_downloads = false
 }
 
 resource github_repository "role-redmine" {
   name          = "ansible-role-redmine"
+  description   = "Redmineをセットアップするロールです"
+  homepage_url  = "https://histudy.jp/"
   has_issues    = true
   has_downloads = false
 }
@@ -98,12 +128,16 @@ resource github_repository "role-redmine" {
 
 resource github_repository "role-apache" {
   name          = "ansible-role-apache"
+  description   = "Apacheをセットアップするロールです"
+  homepage_url  = "https://histudy.jp/"
   has_issues    = true
   has_downloads = false
 }
 
 resource github_repository "role-postgresql" {
   name          = "ansible-role-postgresql"
+  description   = "PostgreSQLをセットアップするロールです"
+  homepage_url  = "https://histudy.jp/"
   has_issues    = true
   has_downloads = false
 }
